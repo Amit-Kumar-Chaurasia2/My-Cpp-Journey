029b6ef595388e33296a7c99eb8d5673ffc30685
Amit Kumar Chaurasia <111045063+imamit-1711@users.noreply.github.com>
2023-03-26 17:31:42 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 4;
+    double b = 1.90;
+    char c = '+';
+
+    int sizeInteger = sizeof(a), sizeDouble = sizeof(b), sizeChar = sizeof(c);
+
+    cout<< "Size of an Integer is "<<sizeInteger<< " bytes"<<endl;
+    cout<< "Size of a Double is "<<sizeDouble<< " bytes"<<endl;
+    cout<< "Size of a Char is "<<sizeChar<< " bytes"<<endl;
+
+}
