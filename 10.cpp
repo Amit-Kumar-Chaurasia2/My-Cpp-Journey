05d0acace4132752dfd83a0e48433eecaba638a2
Amit Kumar Chaurasia <111045063+imamit-1711@users.noreply.github.com>
2023-03-27 01:48:23 +0530

    Add files via upload

---
+++ 10.cpp
@@ -0,0 +1,13 @@
+/* fahrenheit to celcius */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float fahrenheit;
+    cin >> fahrenheit;
+    float celcius = (5.0/9)*(fahrenheit-32);
+    cout<<fahrenheit<<"F = "<<celcius<<" C"<<endl;
+
+    return 0;
+}
